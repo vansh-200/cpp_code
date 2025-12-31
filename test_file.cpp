5e05c01e16a98a7a524b545813635d5f90bc28c8
vansh verma <vanshverma1612011@gmail.com>
2025-12-31 12:25:46 +0530

    Update test_file.cpp

--- test_file.cpp
+++ test_file.cpp
@@ -1,1 +1,17 @@
-this is a test file
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	int arr [8]={2,3,4,1,8,7,9,5};
+	cout <<"this is even number"<<endl;
+	
+	for(int i = 0;i<8;i++){
+	
+	
+	if (arr [i] % 2==0){
+		cout << arr[i]<<" "<<endl;
+	}
+}
+	return 0;
+}
